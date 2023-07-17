3a437f7ffa1808626e4a3c3ade1875e1b1af88f9
Dmitriy <110903946+DmitriyLeonovSPb@users.noreply.github.com>
2023-07-17 10:26:24 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from recursion_directory import rec_dir
+from TxtToJsonConverter import txt_to_json
+from UserCycle import get_from_user
+from JsonToCsv import get_from_user2
+from CsvToJson import csv_to_json
+
+if __name__ == '__main__':
+    rec_dir()
+    txt_to_json()
+    get_from_user()
+    get_from_user2()
+    csv_to_json()
+
+
+
---
+++ test.py
@@ -0,0 +1,24 @@
+import os
+import csv
+import json
+import pickle
+f = open('bufer.txt','w')
+for root, dirs, files in os.walk("."):
+    path = root.split(os.sep)
+    f.write((len(path) - 1) * '---' + os.path.basename(root) +
+             " Folder size: " + str(os.path.getsize(root)) + " bytes " + "\n")
+    for file in files:
+        f.write(len(path) * '---' + file + " File size: " +
+                 str(os.path.getsize(file)) + " bytes " + "\n")
+print("Дерево сохранено в папку bufer.txt")
+f.close() 
+f = open('bufer.txt','r')
+contentt = f.readlines()
+with open("data.json", "w") as write_file:
+   json.dump(contentt, write_file)
+with open("data.csv", mode="w", encoding='utf-8') as w_file:
+    file_writer = csv.writer(w_file, delimiter = "/", lineterminator="\r")
+    file_writer.writerow(contentt)       
+with open('data.pickle', 'wb') as f:
+    pickle.dump(contentt, f)
+f.close() 
